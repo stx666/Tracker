a254948d1f8175ad98763db81af2f2defcfd5f2c
Simon Tournay <simontournay@gmail.com>
2021-03-31 17:35:32 +0100

    add json load with sortable columns

--- js/getdata.js
+++ js/getdata.js
@@ -1,10 +1,15 @@
+let $table = $('#table')
+let tabledata = [];
 let xmlhttp = new XMLHttpRequest();
+let tmp1 = {};
+
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         let data = JSON.parse(this.responseText).feed.entry;
         let i;
+
         for (i = 0; i < data.length; i++) {
-            let row = data[i];
+            // let row = data[i];
             let event = data[i]["gsx$event"]["$t"];
             let startdate = data[i]["gsx$startdate"]["$t"];
             let eventlength = data[i]["gsx$eventlength"]["$t"];
@@ -14,36 +19,22 @@ xmlhttp.onreadystatechange = function () {
             let link = data[i]["gsx$facebookdiscord"]["$t"];
             let tableto = data[i]["gsx$tabletopto"]["$t"];
 
-            document.getElementById("demo").innerHTML +=
-                "<tr>" +
-                "<td>" +
-                event +
-                "</td>" +
-                "<td>" +
-                startdate +
-                "</td>" +
-                "<td>" +
-                eventlength +
-                "</td>" +
-                "<td>" +
-                format +
-                "</td>" +
-                "<td>" +
-                zone +
-                "</td>" +
-                "<td>" +
-                notes +
-                "</td>" +
-                "<td>" +
-                link +
-                "</td>" +
-                "<td>" +
-                tableto +
-                "</td>" +
-                "</tr>";
-        }
-    }
-};
+            tabledata.push({
+                'event': event,
+                'startdate': startdate,
+                'eventlength':eventlength ,
+                'format': format,
+                'zone': zone,
+                'notes': notes,
+                'link': link,
+                'tableto':tableto
+            });
+        };
+
+        debugger;
+        $table.bootstrapTable('load', tabledata);
+    };
+}
 
 xmlhttp.open("GET", "https://spreadsheets.google.com/feeds/list/1oM0L9V1yrfgT2pj0oEzNE3YC9XVYsGPp9-tQ4LFaURE/od6/public/values?alt=json", true);
 xmlhttp.send();
