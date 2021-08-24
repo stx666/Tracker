ae95b42bf58fc50a717a2236f8c14eee8be34c6b
Simon Tournay <simontournay@gmail.com>
2021-08-24 17:47:24 +0100

    using ISOdate takes the date back as it thinks its UTC fix

--- js/getdata.js
+++ js/getdata.js
@@ -72,11 +72,7 @@ fetch(
     for (let i = 0; i < data.length; i++) {
       let event = data[i].c[0] === null ? "" : data[i].c[0].v;
       let startdate = data[i].c[1] === null ? "" : data[i].c[1];
-<<<<<<< HEAD
-      let eventDate = new Date(startdate.f);
-=======
       let eventDate = new Date(startdate.f + " UTC");
->>>>>>> origin/main
       let eventlength = data[i].c[2] === null ? "" : data[i].c[2].v;
       let format = data[i].c[3] === null ? "" : data[i].c[3].v;
       let zone = data[i].c[4] === null ? "" : data[i].c[4].v;
